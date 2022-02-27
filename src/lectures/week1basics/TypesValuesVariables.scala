6d6b7d48539b1851dbd68cc20c723c5b00ad8c11
Yuri Dadteev <48838615+yuridadt@users.noreply.github.com>
2022-02-27 16:15:03 +0300

    TypesValuesVariables

--- src/lectures/week1basics/TypesValuesVariables.scala
+++ src/lectures/week1basics/TypesValuesVariables.scala
@@ -2,4 +2,18 @@ package lectures.week1basics
 
 object TypesValuesVariables extends App{
 
+  var aString: String = "Hello"
+  val aChar: Char = 'C'
+  val aInt: Int = 11
+  val aLong: Long = 11L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 2.0
+  val aBoolean: Boolean = true
+  println(aString,aChar,aInt,aLong,aFloat,aDouble,aBoolean)
+  println(s"Класс для aLong ${aLong} = ${aLong.getClass}")
+
+  // Изменяем только перменные с var, не val
+  aString = "Hello, World"
+  // Через ; пишем только если неск-ко выраженйи на 1-й строке
+  val aaChar: Char = 'C';  val aaInt: Int = 11
 }
