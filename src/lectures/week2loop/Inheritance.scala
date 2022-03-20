fd0aeb2e609adb1b8c8350f6023deed12511dc1d
Yuri Dadteev <48838615+yuridadt@users.noreply.github.com>
2022-03-20 21:34:33 +0300

    Inheritance part1

--- src/lectures/week2loop/Inheritance.scala
+++ src/lectures/week2loop/Inheritance.scala
@@ -1,5 +1,40 @@
 package lectures.week2loop
 
 object Inheritance extends App {
+  class Person(name: String, age: Int) {
+    val gender = "n/a"
+    def greet: String = s"Hello"
+//    private нельзя использовать вне класса
+    private def greet1 : String = "Hello private"
+    protected def greet2: String = "Hello protected"
 
+//    Чтобы не писасть при опредлении дочернего класса параметры родительского в extends
+    def this() = this("UnknownPerson", 0)
+
+    println(greet1)
+  }
+
+  class Student(name: String, age: Int, id: Int, override val gender: String) extends Person(name,age) {
+//    override def greet: String = s"Hello, $name"
+//    Можно использовать вызов метода родительского класса и добавление туда своего черех super
+    override def greet: String = s"${super.greet}, $name"
+    println(greet2)
+  }
+
+  val student = new Student("Xi", 22, 1, "m")
+  println(student.greet)
+  println(student.gender)
+
+//  задачка
+  class Button(label: String){
+  def click(): String = s"button -$label- has been clicked"
+
+  def this() = this("test")
+}
+  class RoundedButton(label:String) extends Button(label) {
+    override def click: String = s"rounded ${super.click()}"
+  }
+
+  val battn = new RoundedButton("1")
+  println(battn.click)
 }
